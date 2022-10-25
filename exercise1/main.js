aebd207e2bd52d024a8a35a51dad1e50b793e6a6
OsmarAlvarado <osmar.alvarado.dev@gmail.com>
2022-10-25 18:16:40 +0200

    folders

---
+++ exercise1/main.js
@@ -0,0 +1,39 @@
+
+//for (let i = 0; i <= 50; i++) {
+//    console.log(i)
+//}
+
+//for (let i = 0; i <= 100; i++) {
+
+
+//    if (i % 5 === 0) {
+//        continue
+//    }
+
+//    if (i % 10 === 0 || i % 15 === 0) {
+
+//        continue
+//    }
+
+//    console.log(i);
+//}
+
+//for (let i = 0; i <= 50; i++) {
+//    if (i % 10 === 0 || i % 15 === 0) {
+//        console.log("Donkey!")
+
+//        continue
+//    }
+//    console.log(i)
+//}
+
+//for (let i = 0; i <= 50; i++) {
+//    if (!i % 2 === 0 && !i % 10 === 0) {
+//        console.log("Monkey!")
+
+//        continue
+//    }
+//    console.log(i)
+//}
+
+
---
+++ exercise2/main.js
@@ -0,0 +1,16 @@
+//n = prompt("introduce un numero")
+//for (let i = 1; i <= n; i++) {
+//    console.log(i)
+//}
+
+//n = prompt("introduce un numero")
+//for (let i = 1; i <= n; i++) {
+//    console.log(i++)
+//}
+  
+//----------HASTA AQUI PUDE LLEGAR------
+
+//n = prompt("introduce un numero")
+//for (let i = 1; i <= n; i++) {
+//    console.log(n - i)
+//}
